f602723dffd729364c824666d348e97bbe4183b4
Gabriel Melo <gabrieldemell1@outlook.com>
2023-11-15 17:59:39 -0300

    feat: structure of application leason

--- src/server.js
+++ src/server.js
@@ -1,31 +1,27 @@
 import http from'node:http'
-// o prefixo node: na importação de um módulo serve para informar que este módulo é interno do Node.JS
-// CommonJS => require
-// ESModules => import/export
 
-// - Criar usuários
-// - Listagem de usuários
-// - Edição de usuários
-// - Remoção de usuários
-
-// - HTTP
-//   - Método HTTP
-//   - URL
-
-// GET, POST, PUT, PATCH, DELETE
-
-// GET => Buscar um recurso do back-end
-// POST => Criar um recurso no back-end
-// PUT => Atualizar um recurso no back-end
-// PATCH => Atualizar uma informação específica de um recurso no back-end
-// DELETE => Deletar um recurso do back-end
-
+const users = []
 
 const server = http.createServer((req, res) => {
     const { method, url } = req
 
-    console.log(method, url)
-    return res.end('Hello Gabriel')
+    if(method ===  'GET' && url === '/users'){
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+
+    if(method ===  'POST' && url === '/users'){
+        users.push({
+            id: 1,
+            name: 'John Doe', 
+            email: 'johndoe@exemple.com'
+        })
+
+        return res.writeHead(201).end()
+    }
+
+    return res.writeHead(404).end('Not Found')
 })
 
-server.listen(3333)
+server.listen(3000)
